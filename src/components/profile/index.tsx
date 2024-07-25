9ac0238a53f2a0dfdee1b6470a811068416cf674
imratatouille <a83284007@gmail.com>
2024-07-25 17:47:09 +0900

    feat: add img and fix styles

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import * as S from "./styled";
 
 export const Profile: React.FC = () => {
-  return (
-    <S.profileContainer>
-      <S.profileBox>
-        <S.ProfileImgBox>이미지~</S.ProfileImgBox>
-        <S.ProfileNameBox>닉네임~~</S.ProfileNameBox>
-      </S.profileBox>
-    </S.profileContainer>
-  );
+    return (
+        <S.profileContainer>
+            <S.profileBox>
+                <S.ProfileImgBox>
+                    <img src='src/assets/nomalprofile.png' width={80}></img>
+                </S.ProfileImgBox>
+                <S.ProfileNameBox>sql injection</S.ProfileNameBox>
+            </S.profileBox>
+        </S.profileContainer>
+    );
 };
--- src/components/profile/styled.ts
+++ src/components/profile/styled.ts
@@ -2,8 +2,13 @@ import styled from "styled-components";
 
 export const profileContainer = styled.div``;
 
-export const profileBox = styled.div``;
+export const profileBox = styled.div`
+    display: flex;
+`;
 
 export const ProfileImgBox = styled.div``;
 
-export const ProfileNameBox = styled.div``;
+export const ProfileNameBox = styled.div`
+    margin-left: 30px;
+    font-size: 30px;
+`;
