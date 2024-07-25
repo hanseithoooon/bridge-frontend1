8171b1c39ff030e176d2e5230a186a83b57ddf63
phph1211 <bagkan1211@gmail.com>
2024-07-25 14:20:47 +0900

    asd

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import * as S from "./styled";
+import { Article } from "../../components/common/article";
 
 export const HomePage: React.FC = () => {
-  return <p>asd</p>;
+  return (
+    <>
+      <S.HomeMainText>
+        가장 인기있는 <br /> 게시물들이에요!
+      </S.HomeMainText>
+      <Article selectedCategory={"정보"} />
+    </>
+  );
 };
---
+++ src/pages/home/styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const HomeMainText = styled.p`
+  margin-top: 20px;
+  font-size: 40px;
+  font-weight: bold;
+`;
