7d26ca737a84cdafef28c1651136a54426f9a550
imratatouille <a83284007@gmail.com>
2024-07-25 14:33:56 +0900

    fix: fix register styled.ts

--- src/pages/register/styled.ts
+++ src/pages/register/styled.ts
@@ -1,75 +1,82 @@
 import styled from "styled-components";
 
 export const registerContainer = styled.div`
-  margin-top: 50%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 30px;
+    margin-top: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 30px;
 `;
 
 export const registerIntroduce = styled.div`
-  font-size: large;
+    margin-bottom: 10px;
+    font-size: large;
 `;
 
 export const registerInputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
 `;
 
 export const registerInputId = styled.input`
-  border: none;
-  border-bottom: solid 1px #ccc;
+    margin-bottom: 10px;
+    border: none;
+    border-bottom: solid 1px #ccc;
 
-  &:focus {
-    outline: none;
-  }
+    &:focus {
+        outline: none;
+    }
 `;
 
 export const registerInputPw = styled.input`
-  border: none;
-  border-bottom: solid 1px #ccc;
+    margin-bottom: 10px;
+    border: none;
+    border-bottom: solid 1px #ccc;
 
-  &:focus {
-    outline: none;
-  }
+    &:focus {
+        outline: none;
+    }
 `;
 
 export const registerInputName = styled.input`
-  border: none;
-  border-bottom: solid 1px #ccc;
+    margin-bottom: 10px;
+    border: none;
+    border-bottom: solid 1px #ccc;
 
-  &:focus {
-    outline: none;
-  }
+    &:focus {
+        outline: none;
+    }
 `;
 
 export const registerInputBirth = styled.input`
-  border: none;
-  border-bottom: solid 1px #ccc;
+    margin-bottom: 10px;
+    border: none;
+    border-bottom: solid 1px #ccc;
 
-  &:focus {
-    outline: none;
-  }
+    &:focus {
+        outline: none;
+    }
 `;
 
 export const registerInputEmail = styled.input`
-  border: none;
-  border-bottom: solid 1px #ccc;
+    margin-bottom: 10px;
+    border: none;
+    border-bottom: solid 1px #ccc;
 
-  &:focus {
-    outline: none;
-  }
+    &:focus {
+        outline: none;
+    }
 `;
 
 export const registerInputPhone = styled.input`
-  border: none;
-  border-bottom: solid 1px #ccc;
+    margin-bottom: 10px;
+    border: none;
+    border-bottom: solid 1px #ccc;
 
-  &:focus {
-    outline: none;
-  }
+    &:focus {
+        outline: none;
+    }
 `;
 
 export const registerInputSubmit = styled.button``;
