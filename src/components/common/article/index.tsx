0ce298586bcdbafa859f2f549f485220dc79232e
imratatouille <a83284007@gmail.com>
2024-07-25 15:21:00 +0900

    fix: fix article styles and add icon

--- src/components/common/article/index.tsx
+++ src/components/common/article/index.tsx
@@ -3,43 +3,42 @@ import * as S from "./styled";
 import { ArticleDummy } from "../../../constant/articleDummy";
 
 interface ArticleProps {
-  selectedCategory: string;
+    selectedCategory: string;
 }
 
 export const Article: React.FC<ArticleProps> = ({ selectedCategory }) => {
-  const filteredArticles =
-    selectedCategory === "게시판"
-      ? ArticleDummy.main.map((item, index) => ({
-          main: item,
-          sub: ArticleDummy.sub[index],
-          category: ArticleDummy.category[index],
-        }))
-      : ArticleDummy.main
-          .map((item, index) => ({
-            main: item,
-            sub: ArticleDummy.sub[index],
-            category: ArticleDummy.category[index],
-          }))
-          .filter((article) => article.category === selectedCategory);
+    const filteredArticles =
+        selectedCategory === "게시판"
+            ? ArticleDummy.main.map((item, index) => ({
+                  main: item,
+                  sub: ArticleDummy.sub[index],
+                  category: ArticleDummy.category[index],
+              }))
+            : ArticleDummy.main
+                  .map((item, index) => ({
+                      main: item,
+                      sub: ArticleDummy.sub[index],
+                      category: ArticleDummy.category[index],
+                  }))
+                  .filter((article) => article.category === selectedCategory);
 
-  return (
-    <S.ArticleContainer>
-      {filteredArticles.map((article, index) => (
-        <S.ArticleItem key={index}>
-          <S.ArticleIcon>📌</S.ArticleIcon>
-          <div>
-            <S.ArticleMainText>
-              {article.main}
-              {ArticleDummy.isNew[index] ? (
-                <S.ArticleNewIcon>🆕</S.ArticleNewIcon>
-              ) : (
-                ""
-              )}
-            </S.ArticleMainText>
-            <S.ArticleSubText>{article.sub}</S.ArticleSubText>
-          </div>
-        </S.ArticleItem>
-      ))}
-    </S.ArticleContainer>
-  );
+    return (
+        <S.ArticleContainer>
+            {filteredArticles.map((article, index) => (
+                <S.ArticleItem key={index}>
+                    <S.anotherCont>
+                        <S.ArticleIcon>📌</S.ArticleIcon>
+                        <div>
+                            <S.ArticleMainText>
+                                {article.main}
+                                {ArticleDummy.isNew[index] ? <S.ArticleNewIcon>🆕</S.ArticleNewIcon> : ""}
+                            </S.ArticleMainText>
+                            <S.ArticleSubText>{article.sub}</S.ArticleSubText>
+                        </div>
+                    </S.anotherCont>
+                    <S.goodIcon>👍</S.goodIcon>
+                </S.ArticleItem>
+            ))}
+        </S.ArticleContainer>
+    );
 };
--- src/components/common/article/styled.ts
+++ src/components/common/article/styled.ts
@@ -1,37 +1,46 @@
 import styled from "styled-components";
 
 export const ArticleContainer = styled.div`
-  width: 100%;
-  height: 100vh;
-  max-height: 100%;
-  overflow-y: auto;
-  overflow-x: hidden;
-  display: flex;
-  flex-direction: column;
-  gap: 30px;
-  margin-top: 20px;
+    width: 100%;
+    height: 100vh;
+    max-height: 100%;
+    overflow-y: auto;
+    overflow-x: hidden;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    margin-top: 20px;
 `;
 
 export const ArticleItem = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 30px;
-  height: 50px;
+    display: flex;
+    align-items: center;
+    gap: 30px;
+    height: 50px;
+    justify-content: space-between;
 `;
 
 export const ArticleMainText = styled.p`
-  font-size: 22px;
-  display: flex;
-  gap: 10px;
+    font-size: 22px;
+    display: flex;
+    gap: 10px;
 `;
 
 export const ArticleSubText = styled.p`
-  font-size: 18px;
-  color: grey;
+    font-size: 18px;
+    color: grey;
 `;
 
 export const ArticleIcon = styled.span`
-  font-size: 22px;
+    font-size: 22px;
 `;
 
 export const ArticleNewIcon = styled.span``;
+
+export const goodIcon = styled.span`
+    font-size: xx-large;
+`;
+
+export const anotherCont = styled.div`
+    display: flex;
+`;
